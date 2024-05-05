1953bef7e5d73b3f01e63aae3a29c6b2e60a9020
MatevzVidovic <matevz.vidovic@gmail.com>
2024-05-05 19:37:03 +0200

    Zahtevki in hover effect

--- src/frontend/src/AdminPage/AdminSideBand.tsx
+++ src/frontend/src/AdminPage/AdminSideBand.tsx
@@ -1,6 +1,14 @@
 import "./AdminSideBand-style.css";
 
 function AdminSideBand() {
+
+    const zahtevki_test = ["Hiša 1", "Podjetje 1", "Podjetje 2", "Podjetje 3", "Hiša 2"];
+
+    for (let i = 0; i < 100; i++) {
+        zahtevki_test.push("Zahtevek " + i);
+    }
+
+
     const handleClick = () => {
         fetch('http://localhost:5000/test', {
             method: 'POST',
@@ -16,13 +24,17 @@ function AdminSideBand() {
         <div className="band">
 
             <div className="group">
-                    <div className="top-button" onClick={handleClick}></div>
-                    <div className="top-button"></div>
+                    <div className="top-button" onClick={handleClick}>Dodaj uporabnika</div>
+                    <div className="top-button">Uredi štartne baze</div>
             </div>
             
             <div className="frame-2">
-                <div className="rectangle"></div>
-                <div className="div"></div>
+                <div className="rectangle">Zahtevki</div>
+                <div className="zahtevki">
+                        {zahtevki_test.map((item, index) => (
+                            <div className="zahtevek" key={index}>{item}</div>
+                        ))}
+                </div>
             </div>
             
         </div>
